b26dc15fc89ffc73952c7ac328c5a212044aaf39
iach526526 <iach526526@gmail.com>
2023-11-22 19:19:36 +0800

    範圍重疊架構

--- b526.微鼓勵/b526_.py
+++ b526.微鼓勵/b526_.py
@@ -5,12 +5,42 @@ def find_next(num,nowINdex,lst):
 PCOUNT,M=[int(input()) for _ in range(2)]#人數,微鼓勵次數
 do_range=[]
 for i in range(M):
-    do_range.extend(list(map(int,input().split(' '))))
-do_range.sort()
+    do_range.append(tuple(map(int,input().split(' '))))
+sorted(do_range)#排序微鼓勵區間，希望可以找到重
 index_ptr=0
 # print(do_range)
 LenM=len(do_range)
 whileCount=0;range_temper=0
+def rmRange(index,count):#給定索引，向右刪除count個元素
+     for _ in range(0,count):
+        do_range.pop(index)
+while(index_ptr<LenM):
+     left=do_range[index_ptr];right=do_range[index_ptr+1]#相鄰兩項出來比較
+     #index 0:起點
+     #index 1:終點
+     if left[0]==right[0]:#起點相同
+            L_end=left[1];R_end=right[1]
+            if L_end>R_end:
+                 '''left範圍較長一點
+                 -----
+                 ---
+                 '''
+                 rmRange(index_ptr,2)#兩項都刪除，用下面那行取代
+                 do_range.insert(index_ptr,(R_end-left[0],R_end))#留多出來那段
+            elif L_end<R_end:
+                 '''left範圍較短一點
+                 ---
+                 -----
+                 '''
+                 rmRange(index_ptr,2)#兩項都刪除，用下面那行取代
+                 do_range.insert(index_ptr,(L_end,R_end))#留多出來那段
+            elif L_end==R_end:#終點也相同
+                 rmRange(index_ptr,2)#兩項都刪除(抵銷)#等於沒做，消除
+    elif : #R
+
+            
+
+
 while(LenM>0):
     L=do_range.pop(0);R=do_range.pop(-1)
     if whileCount%2==0 :
